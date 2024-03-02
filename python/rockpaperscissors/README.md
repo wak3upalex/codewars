4e3bb935f6dfc5632ca9ce7e57e343d03601e9d2
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-03-02 15:28:46 +0300

    # 1 - Rock Paper Scissors

---
+++ python/rockpaperscissors/README.md
@@ -0,0 +1,13 @@
+# Rock Paper Scissors
+## 8 kyu
+### Description
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples(Input1, Input2 --> Output):
+
+```
+"scissors", "paper" --> "Player 1 won!"
+"scissors", "rock" --> "Player 2 won!"
+"paper", "paper" --> "Draw!"
+```
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/python
---
+++ python/rockpaperscissors/main.py
@@ -0,0 +1,8 @@
+# rock paper scissors
+# "scissors", "paper" --> "Player 1 won!"
+# "scissors", "rock" --> "Player 2 won!"
+# "paper", "paper" --> "Draw!"
+def rps(p1, p2):
+    return("Draw!" if p1 == p2 else "Player 1 won!" if (p1 == "scissors" and p2 == "paper") or
+                                                       (p1 == "paper" and p2 == "rock") or
+                                                       (p1 == "rock" and p2 == "scissors") else "Player 2 won!")
