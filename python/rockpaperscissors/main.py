ebce6ff8831f63a267efcf66ac0aa3fc5f9111f6
Aleksei Dmitriev <aleshadmi@mail.ru>
2024-03-03 12:53:20 +0300

    formatting changes

--- python/rockpaperscissors/main.py
+++ python/rockpaperscissors/main.py
@@ -1,4 +1,4 @@
 def rps(p1, p2):
-    return("Draw!" if p1 == p2 else "Player 1 won!" if (p1 == "scissors" and p2 == "paper") or
+    return ("Draw!" if p1 == p2 else "Player 1 won!" if (p1 == "scissors" and p2 == "paper") or
                                                        (p1 == "paper" and p2 == "rock") or
                                                        (p1 == "rock" and p2 == "scissors") else "Player 2 won!")
